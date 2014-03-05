c6dc899c7359aed09d61e9128fcfeb9d0efac8ea
Joe Grossberg <josephgrossberg+github@gmail.com>
2014-03-05 10:40:58 -0500

    copied from Python project at https://github.com/noamraph/tqdm/commit/da5fdbf7c05a9127f04ac5887292457e8335f480

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='tqdm',
+      version='1.0',
+      description='A Simple Python Progress Meter',
+      author='Noam Yorav-Raphael',
+      author_email='noamraph@gmail.com',
+      url='https://github.com/noamraph/tqdm',
+      py_modules=['tqdm'],
+     )
